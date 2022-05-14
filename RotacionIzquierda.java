3604266f3a5109e2b3543b0aca6f80178f67a54c
Gabriel Steven Machicao Quispe <gmachicaoq@unsa.edu.pe>
2022-05-13 22:57:49 -0500

    Se creo la clase RotacionIzquierda, la cual tiene el método main y un método de rotarIzquierdaArray, en el main se introduce los datos.

---
+++ RotacionIzquierda.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class RotacionIzquierda{
+  public static void main (String [] args){ 
+  int[] arr1 ={1,3,9,2,5};
+  int posicion;
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Indique desde que posición:");
+  posicion = sc.nextInt();
+  }
+  public int[] rotarIzquierdaArray(int[] A){
+    int [] Aiz = null;
+    return Aiz;
+  }
+} 
