bf768d22d4b76fd5a165c6f63d4ccc24f73b344d
AnalyQuispe <gquispeb@unsa.edu.pe>
2022-05-14 15:23:25 -0500

    Clase Node básico.

---
+++ Node.java
@@ -0,0 +1,24 @@
+package Lab_02;
+
+public class Node <T>{
+	
+	private T dato;
+	private Node<T> nextNode;
+	
+	public Node(T e) {
+		this.dato = e;
+	}
+	public T getDato() {
+		return dato;
+	}
+	public void setDato(T dato) {
+		this.dato = dato;
+	}
+	public Node<T> getNextNode() {
+		return nextNode;
+	}
+	public void setNextNode(Node<T> nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+}
