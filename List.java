7f1a8d7d6db6bf5d591cc97c2bbad6e7f2fc324c
Quispe Bejar Garlet Analy <64673450+AnalyQuispe@users.noreply.github.com>
2022-05-16 17:51:29 -0500

    Actualizando la clase List

--- List.java
+++ List.java
@@ -12,6 +12,88 @@ public class List<T> {
 	public void clear() {
 		this.root = null;
 	}
+	public void add(T elemento){ //para añadir al final
+
+		Node<T> nodo = new Node<T>(elemento);
+
+		if (root == null)
+			root = nodo;
+		else{
+
+			Node<T> puntero = root;
+
+			while(puntero.getNextNode() != null)
+				puntero = puntero.getNextNode();
+
+			puntero.setNextNode(nodo);
+		}
+		contador++;
+	}
+	
+	public void add(int indice, T elemento){
+
+		if (root == null || indice < 0 || indice > contador - 1)
+			System.out.println("Indice no encontrado");
+		
+		else if (indice == 0){
+			Node<T> nodo = new Node<T>(elemento);
+			nodo.setNextNode(root);
+			root = nodo;
+			contador++;
+		}
+		
+		else if (indice == contador - 1)
+			add(elemento);
+		
+		else {
+			Node<T> nodo = new Node<T>(elemento);
+			Node<T> puntero = root;
+
+			for (int i = 0; i < indice - 1; i++)
+				puntero = puntero.getNextNode();
+		
+			Node<T> auxiliar = puntero.getNextNode();
+			puntero.setNextNode(nodo);
+			nodo.setNextNode(auxiliar);
+
+			contador++;
+		}
+	}
+	
+	public Node<T> get(int indice){ //recibe el indice para extraer el elemento
+
+		Node<T> puntero = root;
+		
+		if (root == null || indice < 0 || indice > contador - 1)
+			puntero = null;
+		
+		else{
+			for (int i = 0; i < indice; i++)
+				puntero = puntero.getNextNode();
+		}
+		return puntero;
+	}
+
+	public void remove(int indice){
+
+		if (root == null || indice < 0 || indice > contador	- 1)
+			System.out.println("La lista esta vacia o no existe el indice");
+		
+		else{
+
+			Node<T> puntero = root;
+
+			fot (int i = 0; i < indice - 1; i++)
+				puntero = puntero.getNextNode();
+
+			Node<T> auxiliar = puntero.getNextNode().getNextNode();
+			puntero.getNextNode().setNextNode(null);
+
+			puntero.setNextNode(auxiliar);
+
+			contador--;
+		}
+	}
 	
 	public List<T> subList(int fromIndex, int toIndex){
 		List<T> subList = new List<T>();
@@ -40,6 +122,8 @@ public class List<T> {
 				indicador = false;
 			}
 			aux = aux.getNextNode();
+			
 		}
+		contador--;
 	}
 }
